0220f80214ecd523896f638b3334cb8ba7612e98
Maxime AILLOUD <maxime.ailloud@gmail.com>
2015-05-06 15:13:50 +0200

    Remove reference to claroline as it's not necessary

--- InnovaAngularJSBundle.php
+++ InnovaAngularJSBundle.php
@@ -6,15 +6,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
 use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 
-class InnovaAngularJSBundle extends Bundle implements AutoConfigurableInterface
+class InnovaAngularJSBundle extends Bundle
 {
-    public function supports($environment)
-    {
-        return true;
-    }
-    
-    public function getConfiguration($environment)
-    {
-        return new ConfigurationBuilder();
-    }
 }
